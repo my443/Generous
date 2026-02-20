9b7e905976ebb3f7a1f41b9e7a6db9afec2cc119
John van Dijk <john@johnv.ca>
2026-02-20 09:45:34 -0500

    You can go to a specific list, or to any list if you want.

---
+++ Generous/Components/Pages/DataPages/Data.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Generous.Models;
+
+namespace Generous.Components.Pages.DataPages
+{
+    public partial class Data
+    {
+        [Parameter] public int DataElementId { get; set; }
+        [Inject] public IDbContextFactory<AppDbContext> DbFactory { get; set; } = default!;
+        [Inject] public IDialogService DialogService { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        private Element Element { get; set; } 
+
+        private List<Element> Elements { get; set; } = new List<Element>();
+
+        protected override async Task OnInitializedAsync() {
+            using var _context = DbFactory.CreateDbContext();
+            Element = await _context.Elements.Where(e => e.Id == DataElementId).FirstOrDefaultAsync();
+            Elements = await _context.Elements.ToListAsync();
+        }
+
+        private async Task ElementClick(FluentDataGridRow<Element> args) {
+            NavigationManager.NavigateTo($"/data/{args.Item.Id}");
+        }
+    }
+}
